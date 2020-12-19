a9bdc5be63cecfb0097ecfc529f727d6d75ac9f6
Pavan Rao <pavanraomr@gmail.com>
2020-12-18 22:47:10 -0500

    completed day 7 part 2

--- day7.py
+++ day7.py
@@ -1,11 +1,40 @@
 
+'''
+--- Day 7: Handy Haversacks ---
 
-# {
-#     'red':[('green', 2), ('blue', 3)]
-#     'yellow':[('blue', 2), ('gold', 3)]
-#     'green':[('yellow', 2), ('gold', 3)]
-# }
+You land at the regional airport in time for your next flight. In fact, it looks like you'll even have time to grab some food: all flights are currently delayed due to issues in luggage processing.
 
+Due to recent aviation regulations, many rules (your puzzle input) are being enforced about bags and their contents; bags must be color-coded and must contain specific quantities of other color-coded bags. Apparently, nobody responsible for these regulations considered how long they would take to enforce!
+
+For example, consider the following rules:
+
+light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.
+
+These rules specify the required contents for 9 bag types. In this example, every faded blue bag is empty, every vibrant plum bag contains 11 bags (5 faded blue and 6 dotted black), and so on.
+
+You have a shiny gold bag. If you wanted to carry it in at least one other bag, how many different bag colors would be valid for the outermost bag? (In other words: how many colors can, eventually, contain at least one shiny gold bag?)
+
+In the above rules, the following options would be available to you:
+
+    A bright white bag, which can hold your shiny gold bag directly.
+    A muted yellow bag, which can hold your shiny gold bag directly, plus some other bags.
+    A dark orange bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+    A light red bag, which can hold bright white and muted yellow bags, either of which could then hold your shiny gold bag.
+
+So, in this example, the number of bag colors that can eventually contain at least one shiny gold bag is 4.
+
+How many bag colors can eventually contain at least one shiny gold bag? (The list of rules is quite long; make sure you get all of it.)
+
+
+'''
 sample = """
 light red bags contain 1 bright white bag, 2 muted yellow bags.
 dark orange bags contain 3 bright white bags, 4 muted yellow bags.
@@ -615,6 +644,23 @@ muted coral bags contain 4 clear red bags, 3 vibrant maroon bags.
 bright lavender bags contain 4 striped blue bags.
 """
 
+"""
+Solution approach: Convert input text to a nested dictionary and then search
+Sample data would be translated to a dictionary like below:
+
+{
+    'bright white': {'shiny gold': '1'},
+    'dark olive': {'dotted black': '4', 'faded blue': '3'},
+    'dark orange': {'bright white': '3', 'muted yellow': '4'},
+    'dotted black': {None: None},
+    'faded blue': {None: None},
+    'light red': {'bright white': '1', 'muted yellow': '2'},
+    'muted yellow': {'faded blue': '9', 'shiny gold': '2'},
+    'shiny gold': {'dark olive': '1', 'vibrant plum': '2'},
+    'vibrant plum': {'dotted black': '6', 'faded blue': '5'}
+}
+"""
+
 import re
 from collections import namedtuple, defaultdict
 bag = namedtuple('bag', 'name count')
@@ -628,12 +674,12 @@ bags = dict()
 
 def split_inner_bags(inner):
     if "no other" in inner:
-        return {None:None}
+        return {None:0}
     else:
         result = dict()
         for inner_bags in inner.split(','):
             x = inner_bags.strip().split(' ', maxsplit=1)
-            result[x[1]] = x[0]
+            result[x[1]] = int(x[0])
         return result
 
 
@@ -660,4 +706,57 @@ def find_gold(d:dict, collected_gold:list = None):
 gold = [1 for k,v in bags.items() if find_gold(v)]
 
 
-print(sum(gold))
+print(f'bag colors can eventually contain at least one shiny gold bag = {sum(gold)}')
+
+''' 
+--- Part Two ---
+
+It's getting pretty expensive to fly these days - not because of ticket prices, but because of the ridiculous number of bags you need to buy!
+
+Consider again your shiny gold bag and the rules from the above example:
+
+    faded blue bags contain 0 other bags.
+    dotted black bags contain 0 other bags.
+    vibrant plum bags contain 11 other bags: 5 faded blue bags and 6 dotted black bags.
+    dark olive bags contain 7 other bags: 3 faded blue bags and 4 dotted black bags.
+
+So, a single shiny gold bag must contain 1 dark olive bag (and the 7 bags within it) plus 2 vibrant plum bags (and the 11 bags within each of those): 1 + 1*7 + 2 + 2*11 = 32 bags!
+
+Of course, the actual rules have a small chance of going several levels deeper than this example; be sure to count all of the bags, even if the nesting becomes topologically impractical!
+
+Here's another example:
+
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.
+
+In this example, a single shiny gold bag must contain 126 other bags.
+
+How many individual bags are required inside your single shiny gold bag?
+
+'''
+
+# pass the bag count as a multiplier every time the function is called
+
+def find_bags_in_me(me:dict, collected_bags:list = None, multiplier=1):
+    if collected_bags is None:
+        all_bags = []
+    else:
+        all_bags = collected_bags
+
+    for k, v in me.items():
+        all_bags.append(v*multiplier)
+        if k is not None:
+            find_bags_in_me(bags[k],all_bags, v*multiplier)
+
+    return all_bags
+
+
+
+bags_in_gold_bag = find_bags_in_me(bags['shiny gold'])
+# print(bags_in_gold_bag)
+print(f'Part 2 - bags in the gold bag = {sum(bags_in_gold_bag)}')
